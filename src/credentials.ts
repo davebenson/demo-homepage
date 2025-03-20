5bfaf7cebdce5abdd5e2f2278b4babd169a8075d
Dave Benson <dave.benson@embodied.me>
2025-03-20 11:51:36 -0700

    ....

--- src/credentials.ts
+++ src/credentials.ts
@@ -2,6 +2,10 @@ import { readEncryptedYaml } from './crypt.ts';
 import fs from 'node:fs/promises';
 import app_env from './app-env.ts';
 
-const key = (await fs.readFile(`config/${app_env}.key`, 'utf-8')).trim();
+
+const env_name = `${app_env.toUpperCase()}_CREDENTIALS_PASSWORD`;
+const key = (env_name in process.env)
+          ? process.env[env_name]
+          : (await fs.readFile(`config/${app_env}.key`, 'utf-8')).trim();
 
 export default await readEncryptedYaml(key, `config/${app_env}.enc`);
